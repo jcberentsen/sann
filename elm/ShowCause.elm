2bb8199d57aef909f9fb3ab84c921360a2ae9689
Jarl Christian Berentsen <jc.berentsen@gmail.com>
2014-10-07 14:07:30 +0200

    Visualization experiments

--- elm/ShowCause.elm
+++ elm/ShowCause.elm
@@ -13,7 +13,31 @@ events : Signal String
 events = connect eventurl events_to_server
 
 step : String -> Element
-step msg = msg |> Json.fromString |> (watch "msg") |> Maybe.maybe "{}" (Json.toString "") |> plainText
+--step msg = msg |> Json.fromString |> (watch "msg") |> Maybe.maybe "{}" (Json.toString "") |> plainText
+step msg = msg |> Json.fromString |> (watch "msg") |> visualize
+
+visualize : Maybe Json.Value -> Element
+visualize v =
+    collage 800 800
+        [ toForm ignorant
+        , move (100,0) (toForm fact)
+        , move (280, 10) (toForm counterfact)
+        , move (10, -160) (toForm cause)
+        ]
+
+ignorant : Element
+ignorant = color grey (container 20 20 middle (plainText "?"))
+
+fact : Element
+fact = color green (container 60 30 middle (plainText "rain!"))
+
+counterfact : Element
+counterfact = color red (container 160 30 middle (plainText "no sprinklers!"))
+
+cause : Element
+cause = flow down
+    [ color yellow (container 60 30 middle (plainText "rain?"))
+    , color orange (container 60 30 middle (plainText "wet?")) ]
 
 main : Signal Element
 main = lift step events
